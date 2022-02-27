96b31319d33c835038264a2b77b07eeb7dea30eb
AngelR24 <49293458+AngelR24@users.noreply.github.com>
2022-02-27 14:53:53 -0400

    First Commit
    All changes.
    Final Version.

---
+++ FibonacciSucession/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FibonacciSucession
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte un número N para imprimir la sucesión Fibonnacci.");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("**************************************");
+            printFibonacci(n);
+            Console.WriteLine("**************************************");
+            Console.WriteLine("Completado.");
+            Console.ReadKey();
+        }
+
+        static void printFibonacci(int n)
+        {
+            int first = 0;
+            int second = 1;
+            Console.WriteLine(first);
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine(second);
+                int sum = first + second;
+                first = second;
+                second = sum;
+            }
+        }
+    }
+}
